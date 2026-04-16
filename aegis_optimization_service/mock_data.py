5acc523ce20c2d790cf6d379771079e38ee9fe1e
Hoang Cong Ly <congglyyy@example.com>
2026-04-16 17:08:02 +0700

    feat(algo): upload Optimization Microservice (Fixed History)

---
+++ aegis_optimization_service/mock_data.py
@@ -0,0 +1,6 @@
+def get_mock_stores():
+    return [
+      { "id": "shop_1", "name": "Nón Lá Việt Traditional", "address": "Chợ Bến Thành", "coords": { "lat": 10.7720, "lng": 106.6983 }, "category": "non la", "price": 80000, "rating": 4.6, "description_raw": "Nón lá truyền thống chuẩn xịn thủ công..." },
+      { "id": "shop_2", "name": "Saigon Denim Store", "address": "Quận 3", "coords": { "lat": 10.7798, "lng": 106.6892 }, "category": "quan jean", "price": 450000, "rating": 4.5, "description_raw": "Quần jean bụi bặm phong cách đường phố..." },
+      { "id": "shop_3", "name": "Minimal Shirt House", "address": "Quận 1", "coords": { "lat": 10.7765, "lng": 106.7000 }, "category": "ao so mi", "price": 300000, "rating": 4.7, "description_raw": "Áo sơ mi lụa mềm mại dạo phố..." }
+    ]
